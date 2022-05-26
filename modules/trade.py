631b9a9172decc5b242941faa0acac21341dc7c4
Dmitry G <sdprecovery@gmail.com>
2022-05-26 16:26:11 +0300

    quickfix

--- modules/trade.py
+++ modules/trade.py
@@ -631,7 +631,7 @@ class Trader(TradeDB, Base):
             print("- Sent whisper to %s : %s" % (current_trade_user[1], current_trade_user[2]))
             self.send_whisper(whisper)
             self.db_update_trade_user_priority(db_conn, current_trade_user, str(datetime.now()))
-            time.sleep(1.5)
+            time.sleep(2)
             self.whisper_queue.task_done()
 
     def run_trader(self, trade_items_file):
