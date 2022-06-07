22fb2d7fb769d5f3d8017490d9bc8e59045c2883
Dmitry G <sdprecovery@gmail.com>
2022-06-07 11:37:25 +0300

    init assets/gui; gui MainApp init_ui

--- modules/gui.py
+++ modules/gui.py
@@ -1,2 +1,40 @@
+import sys
+from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtWidgets import (
+    QApplication,
+    QPushButton,
+    QWidget,
+)
+from PyQt5.QtGui import QIcon
+
+
+class MainApp(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.app_title = 'App title'
+        self.app_icon = 'assets/gui/logo.png'
+
+        self.init_ui()
+
+    def init_ui(self) -> None:
+        # window params
+        self.setWindowTitle(self.app_title)
+        self.setWindowIcon(QIcon(self.app_icon))
+        self.setGeometry(900, 350, 551, 500)  # location x,y and size x,y
+        # ui elements
+        self.ui_quit_btn()
+        # show
+        self.show()
+
+    def ui_quit_btn(self) -> None:
+        btn = QPushButton('Quit', self)
+        btn.move(50, 50)
+        btn.resize(btn.sizeHint())
+        btn.clicked.connect(QCoreApplication.instance().quit)
+
+
 if __name__ == '__main__':
-    pass
+    qt_app = QApplication(sys.argv)
+    ex = MainApp()
+    sys.exit(qt_app.exec_())
