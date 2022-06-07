c10bbe5c102a477c145f3057ea7d9b3419b8bfaf
Dmitry G <sdprecovery@gmail.com>
2022-06-07 14:23:18 +0300

    gui set_main_styles set_window set_window_size set_window_center

--- modules/gui.py
+++ modules/gui.py
@@ -5,9 +5,11 @@ from PyQt5.QtWidgets import (
     QAction,
     qApp,
     QDesktopWidget,
+    QLabel,
     QMainWindow,
     QPushButton,
     QToolTip,
+    QVBoxLayout,
     QWidget,
 )
 from PyQt5.QtGui import QIcon, QFont
@@ -19,28 +21,46 @@ class MainApp(QMainWindow):
 
         self.app_title = 'App title'
         self.app_icon = 'assets/gui/logo.png'
+        self.app_window_size = [550, 500]
 
         self.init_ui()
 
     def init_ui(self) -> None:
         QToolTip.setFont(QFont('Roboto', 10))
         # window
-        self.set_window()
+        self.set_window(size=self.app_window_size)
         # ui
         self.ui_quit_btn()
         # toolbar
         self.set_toolbar()
         # status
         self.set_statusbar()
+        # styles
+        self.set_main_styles()
         # show
         self.show()
+
+    def set_main_styles(self) -> None:
+        """Set styles for main frame/bars"""
+        # self.setStyleSheet('background-color: #121212; color: white;')
+        self.statusBar().setStyleSheet("""
+            background-color: #181818;
+            color: white;
+        """)
 
     def set_window(self, size=[550, 500]) -> None:
         """Set window default title/icon/size/position"""
         self.setWindowTitle(self.app_title)
         self.setWindowIcon(QIcon(self.app_icon))
         # position/size
+        self.set_window_size(size=size)
+        self.set_window_center()
+
+    def set_window_size(self, size=[550, 500]) -> None:
         self.resize(*size)
+        self.setFixedSize(self.size())
+
+    def set_window_center(self) -> None:
         window_rect = self.frameGeometry()  # app window rect
         window_coords = QDesktopWidget().availableGeometry().center()  # screen center x,y
         window_rect.moveCenter(window_coords)
